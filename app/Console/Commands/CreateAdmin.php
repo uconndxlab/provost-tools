8eb7e6d2f21418826f6ca7cafebd68baa911619b
Brian Kelleher <brian.kelleher@uconn.edu>
2024-10-03 18:34:43 -0400

    create admin supports users that don't exist

--- app/Console/Commands/CreateAdmin.php
+++ app/Console/Commands/CreateAdmin.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\User;
+use App\Ldap\LdapUser;
 
 class CreateAdmin extends Command
 {
@@ -27,6 +28,26 @@ class CreateAdmin extends Command
     public function handle()
     {
         $user = User::where('netid', $this->argument('netid'))->first();
+
+        if ( !$user ) {
+            $this->info('User not found. Creating user.');
+            $ldapUser = LdapUser::where('uid', $this->argument('netid'))->first();
+
+            if ( $ldapUser ) {
+                $user = User::create([
+                    'netid' => $this->argument('netid'),
+                    'name' => $ldapUser->cn[0],
+                    'email' => $ldapUser->mail[0],
+                    // 'emplid' => $ldapUser->employeeNumber[0], // Not in our LDAP attribute set currently
+                    'affiliation' => $ldapUser->edupersonaffiliation[array_key_last($ldapUser->edupersonaffiliation)],
+                    'title' => $ldapUser->title[0],
+                ]);
+                $this->info('User created.');
+            } else {
+                $this->error('User not found.');
+                return;
+            }
+        }
 
         if ( $this->option('remove') ) {
             $user->can_admin = false;
