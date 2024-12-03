4cdb48d1303666b1375a847576ed979278e23611
Joel Salisbury <joel.saisbury@gmail.com>
2024-12-03 10:23:21 -0500

    stub of gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use App\Models\SchoolCollege;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define('faculty-salary-tables-view', function (User $user) {
             return $user->id === $post->user_id;
+        });
+
+        Gate::define('can_create_budget_questionnaire', function (User $user, SchoolCollege $schoolCollege) {
+            return $schoolCollege->usersWithPermission('create_budget_questionnaire')->contains($user);
         });
     }
 }
