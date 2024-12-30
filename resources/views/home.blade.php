0abd17a19a946d2f68894f082f89d2a515b005bf
Joel Salisbury <joel.saisbury@gmail.com>
2024-12-30 10:35:49 -0500

    looks more like what natalie said

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,11 @@
     <div>
         <div class="hero text-center text-white py-5">
             <div class="container">
-                <h1>Welcome to POET</h1>
+                <h1>Welcome to 
+
+              <abbr title="Provost's Operational Efficiency Toolkit">POET</abbr>
+                </h1>
+                <h4>The Provost's Operational Efficiency Toolkit</h4>
                 <div class="poet-line-container">
                     <div class="poet-line">
                         <div class="poet-dot"></div>
@@ -19,47 +23,49 @@
                         <span>Empower Decision-making</span>
                     </div>
                 </div>
-                
-                <a href="#tools" class="btn btn-lg btn-light mt-4 shadow">
-                    See all Tools <i class="fas fa-arrow-down ms-2"></i>
-                </a>
             </div>
         </div>
-        
 
-        <div class="container">
+        <div class="content-wrap">
 
-            <div id="tools" class="row mt-5">
-                <div class="col-md-6 mb-4">
-                    <div class="card">
-                        <div class="card-header bg-primary text-white">
-                            <h3>Annual Budget Discussion – Unit Submission</h3>
-                        </div>
-                        <div class="card-body">
-                            <p>Streamlined submission tool that creates an outline for annual budget hearing meetings. Discussions will center around how units will meet the budget guidance, while striving towards strategic plan goals.</p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="{{ route('budgetHearingQuestionnaire.create') }}" class="btn btn-primary">
-                                Submit the Questionnaire
-                            </a>
+
+            <div class="container">
+
+                <div id="tools" class="row mt-5">
+                    <div class="col-md-3 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="material-icons">content_paste</i>
+                                <h3>Annual Budget Discussion – Unit Submission</h3>
+
+                                <p>Streamlined submission tool that creates an outline for annual budget hearing meetings.
+                                    Discussions will center around how units will meet the budget guidance, while striving
+                                    towards strategic plan goals.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="{{ route('budgetHearingQuestionnaire.create') }}" class="tool-link">
+                                    Submit the Questionnaire
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6 mb-4">
-                    <div class="card" style="opacity: 0.5;">
-                        <div class="card-header bg-primary text-white">
-                            <h3>Faculty Salary Tables</h3>
-                            <span class="badge bg-danger">Available April 1- Jun 1</span>
-                        </div>
-                        <div class="card-body">
+                    <div class="col-md-3 mb-4">
+                        <div class="card">
 
-                            <p>Salary data for faculty within the AAUP, in accordance with the annual Provost Fund Guidance.</p>
+                            <div class="card-body">
+                                <i class="material-icons">paid</i> <br>
 
-                        </div>
-                        <div class="card-footer">
-                            <a href="{{ route('faculty_salary_tables.index') }}" class="btn btn-primary disabled">
-                                View your Department's Salary Information
-                            </a>
+                                <span class="badge bg-info text-primary">Available April 1- Jun 1</span>
+
+                                <h3>Faculty Salary Tables</h3>
+
+                                <p>Salary data for faculty within the AAUP, in accordance with the annual Provost Fund
+                                    Guidance.</p>
+
+                            </div>
+                            <div class="card-footer">
+                                <small>Available April 1 - June 1</small>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,9 @@
     {{--
     <link rel="stylesheet" href="style.css"> --}}
     <link rel="stylesheet" href="/banner.css">
+    {{-- material design icons --}}
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
     {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
 
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
@@ -23,18 +26,21 @@
         <x-uconn-banner />
         <header>
             @php $is_admin = Route::is('admin*'); @endphp
-            <div @class(["bg-provost-grey py-4 text-black", "border-bottom border-danger border-5"=> $is_admin])>
+            <div @class([
+                'py-4 text-black',
+                'border-bottom border-danger border-5' => $is_admin,
+            ])>
                 <div class="container d-flex align-items-center justify-content-between">
                     <div>
                         <p class="header-level-two">
-                            <a href="https://provost.uconn.edu/" 
-                                class="link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-offset-2">
+                            <a href="https://provost.uconn.edu/"
+                                class="link-offset-2">
                                 Office of the Provost
                             </a>
                         </p>
                         <h1 class="header-level-one">
-                            <a href="/" 
-                                class="link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-offset-1">
+                            <a href="/"
+                                class="link-offset-1">
                                 Provost's Operational Efficiency Toolkit
                             </a>
                         </h1>
@@ -42,69 +48,77 @@
                     <!-- Navigation Menu -->
                     <nav class="navbar navbar-expand-lg">
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#provostToolsMainNav" aria-controls="provostToolsMainNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
+                            data-bs-target="#provostToolsMainNav" aria-controls="provostToolsMainNav"
+                            aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="provostToolsMainNav">
                             <ul class="navbar-nav ms-auto">
-                                @if ( !$is_admin )
-                                <li class="nav-item dropdown">
-                                    <a href="#" @class(["nav-link dropdown-toggle", "active"=> Route::is('faculty*')])
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Available Tools
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="{{route('budgetHearingQuestionnaire.create')}}" class="dropdown-item">
-                                                Budget Hearing Questionnaire
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    {{-- login --}}
-                                    @if (Auth::check())
-                                    <form action="{{ route('logout') }}" method="POST">
-                                        @csrf
-                                        <button type="submit" class="btn btn-link nav-link">Logout</button>
-                                    </form>
-                                    @else
-                                    <a href="{{ route('login') }}" class="nav-link">Login</a>
-                                    @endif
-                                </li>
+                                @if (!$is_admin)
+                                    <li class="nav-item dropdown">
+                                        <a href="#" @class([
+                                            'nav-link dropdown-toggle',
+                                            'active' => Route::is('faculty*'),
+                                        ]) role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Available Tools
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li>
+                                                <a href="{{ route('budgetHearingQuestionnaire.create') }}"
+                                                    class="dropdown-item">
+                                                    Budget Hearing Questionnaire
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        {{-- login --}}
+                                        @if (Auth::check())
+                                            <form action="{{ route('logout') }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-link nav-link">Logout</button>
+                                            </form>
+                                        @else
+                                            <a href="{{ route('login') }}" class="nav-link">Login</a>
+                                        @endif
+                                    </li>
                                 @else
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('home') }}">Return to Site</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a @class(['nav-link', 'active'=> Route::is('admin.home') ]) href="{{ route('admin.home') }}">
-                                        Admin
-                                    </a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a href="#" @class(["nav-link dropdown-toggle", "active"=> Route::is('admin.faculty*')])
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Tools for Faculty
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="{{ route('admin.faculty_salary_tables.index') }}" class="dropdown-item">
-                                                Salary Tables
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ route('admin.budgetHearingQuestionnaire.index') }}" class="dropdown-item">
-                                                Budget Hearing Questionnaire
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a @class(['nav-link', 'active'=> Route::is('admin.users.index') ]) 
-                                        href="{{ route('admin.users.index') }}">Users
-                                    </a>
-                                </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('home') }}">Return to Site</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a @class(['nav-link', 'active' => Route::is('admin.home')]) href="{{ route('admin.home') }}">
+                                            Admin
+                                        </a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a href="#" @class([
+                                            'nav-link dropdown-toggle',
+                                            'active' => Route::is('admin.faculty*'),
+                                        ]) role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Tools for Faculty
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li>
+                                                <a href="{{ route('admin.faculty_salary_tables.index') }}"
+                                                    class="dropdown-item">
+                                                    Salary Tables
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="{{ route('admin.budgetHearingQuestionnaire.index') }}"
+                                                    class="dropdown-item">
+                                                    Budget Hearing Questionnaire
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a @class(['nav-link', 'active' => Route::is('admin.users.index')]) href="{{ route('admin.users.index') }}">Users
+                                        </a>
+                                    </li>
                                 @endif
                             </ul>
                         </div>
@@ -113,44 +127,46 @@
             </div>
         </header>
     </div>
-    
-    
-    <main>
-        @if ( session('success') )
-        <div class="container pt-4">
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        </div>
-        @endif
-        @if ( session('message') )
-        <div class="container pt-4">
-            <div class="alert alert-warning">
-                {{ session('message') }}
-            </div>
-        </div>
-        @endif
 
-        @if ( $errors->any() )
 
-        <div class="container pt-4">
-            <div class="alert alert-danger">
-                @foreach ($errors->all() as $error)
-                <p class="mb-1">{{ $error }}</p>
-                @endforeach
-            </div>
+    <main class="curved-main">
+        <div class="main-content">
+            @if (session('success'))
+                <div class="container pt-4">
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                </div>
+            @endif
+            @if (session('message'))
+                <div class="container pt-4">
+                    <div class="alert alert-warning">
+                        {{ session('message') }}
+                    </div>
+                </div>
+            @endif
+
+            @if ($errors->any())
+
+                <div class="container pt-4">
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <p class="mb-1">{{ $error }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+
+            @if (session('error'))
+                <div class="container pt-4">
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                </div>
+            @endif
+
+            @yield('content')
         </div>
-        @endif
-
-        @if ( session('error') )
-        <div class="container pt-4">
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        </div>
-        @endif
-
-        @yield('content')
     </main>
     <footer class="text-bg-dark d-block w-100 py-4 d-print-none">
         <div class="container">
