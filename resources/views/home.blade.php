33f755b538104909af6bcea096f425cbab59391c
Joel Salisbury <joel.saisbury@gmail.com>
2024-12-31 13:42:20 -0500

    equal height cards, animation on load

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,6 +100,31 @@
                 </div>
             </div>
         </div>
-
     </div>
+
+    <script>
+        // equalize card heights in #tools
+        document.addEventListener('DOMContentLoaded', function() {
+            function equalizeCardHeights() {
+            var cards = document.querySelectorAll('.card');
+            var maxHeight = 0;
+
+            cards.forEach(function(card) {
+                card.style.height = 'auto'; // Reset height
+                if (card.offsetHeight > maxHeight) {
+                maxHeight = card.offsetHeight;
+                }
+            });
+
+            cards.forEach(function(card) {
+                card.style.height = maxHeight + 'px';
+            });
+            }
+
+            equalizeCardHeights();
+            window.addEventListener('resize', equalizeCardHeights);
+        });
+        
+    </script>
+
 @endsection
