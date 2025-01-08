e9c8c2816b8537dc73e92d52c619cd9c90ca3df8
Joel Salisbury <joel@uconn.edu>
2025-01-08 13:23:20 -0500

    style tweaks

--- resources/views/decision_maker/institutional_priorities/index.blade.php
+++ resources/views/decision_maker/institutional_priorities/index.blade.php
@@ -4,13 +4,14 @@
 <div class="py-5">
     <div class="container">
         <h1>Institutional Priorities</h1>
-        <p>These are the institutional priorities that have been set for the current academic year.</p>
+        <p>These are the institutional priorities that have been set for the organization.</p>
         <a href="{{ route('institutional_priorities.create') }}" class="btn btn-primary">Add Priority</a>
         <div class="table-responsive mt-3">
             <table class="table">
                 <thead>
                     <tr>
                         <th scope="col">Priority</th>
+                        <th scope="col">Name</th>
                         <th scope="col">Description</th>
                         <th scope="col">Actions</th>
                     </tr>
@@ -19,13 +20,14 @@
                     @foreach($priorities as $priority)
                     <tr>
                         <td>{{ $priority->weight }}</td>
+                        <td>{{ $priority->name }}</td>
                         <td>{{ $priority->description }}</td>
                         <td>
                             <a href="{{ route('institutional_priorities.edit', $priority) }}" class="btn btn-primary">Edit</a>
                             <form action="{{ route('institutional_priorities.destroy', $priority) }}" method="POST" style="display: inline;">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
+                                <button type="submit" class="btn btn-danger mt-2">Delete</button>
                             </form>
                         </td>
                     </tr>
