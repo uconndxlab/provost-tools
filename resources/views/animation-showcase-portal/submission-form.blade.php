b998426e0c9b93a355eb672b1d88fdf7934a79fb
Natalie L <2018Lacroixn@gmail.com>
2025-02-03 13:19:34 -0500

    animation showcase hero

--- resources/views/animation-showcase-portal/submission-form.blade.php
+++ resources/views/animation-showcase-portal/submission-form.blade.php
@@ -1,17 +1,21 @@
 @extends('layout')
 @section('title', 'Contact')
 @section('content')
-<div class="hero text-center text-white py-5">
+<div class="hero text-center text-white py-5 animation-hero">
     <div class="container">
-        <h1>2025 Big East Animation Showcase
+        <h1>2025 Big East <br>Animation Showcase
         </h1>
-        <h2>Submit a thingy</h2>
+        <h2 style="font-style:normal">This could be a subtitle</h2>
+        <p class="memo">COMING SOON</p>
+
     </div>
+    <img class="decorative" src="{{url('/images/decorative.svg')}}" alt="">
 </div>
-<div class="content-wrap">
+<div class="form">
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-8 offset-md-2">
+            <h2>Submission Form Fields</h2>
             <form action="#" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
@@ -32,4 +36,63 @@
         </div>
     </div>
 </div>
+<style>
+    .animation-hero{
+        background:linear-gradient(0deg, rgba(1, 62, 205, 0.4), rgba(1, 62, 205, 0.5)), url("/images/animation-photo.png");
+        background-position:center center;
+        background-color:rgba(0,0,0,0.2);
+        position:relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        overflow:visible;
+    }
+    .animation-hero .container{
+        position:relative;
+        z-index:3;
+        padding:75px 0px;
+        min-height:400px;
+    }
+    .animation-hero h1, .animation-hero h2{
+        color:#fff;
+        text-shadow:0px 1px 6px rgba(0,0,0,0.7);
+        padding-bottom:10px;
+    }
+    .memo{
+        font-size:20px;
+        background-color:#000e2f;
+        width:max-content;
+        margin:10px auto 20px auto;
+        padding:5px 15px;
+    }
+
+    .decorative{
+        user-drag: none;
+        -webkit-user-drag: none;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%) translateY(40%);
+        width:100%;
+    }
+
+    .form{
+        position:relative;
+        top:0px;
+        padding:80px 0px;
+    }
+
+    .form h2{
+        font-family: georgiapro, serif;
+        color: #013ECD;
+        font-weight:500;
+        text-align:center;
+    }
+
+</style>
 @endsection
