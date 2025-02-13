978654c50057dd788786ab81d54c2e1c2d73b39f
Joel Salisbury <joel.saisbury@gmail.com>
2025-02-13 15:06:39 -0500

    nav fix

--- resources/views/decision_maker/parts/nav.blade.php
+++ resources/views/decision_maker/parts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="app-nav navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Decision Maker</a>
+        <a class="navbar-brand" href="{{ route('decision_maker.projects.index') }}">Decision Maker</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,9 +11,6 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('decision_maker.institutional_priorities.index') }}">Institutional Priorities</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Admin</a>
                 </li>
             </ul>
         </div>
