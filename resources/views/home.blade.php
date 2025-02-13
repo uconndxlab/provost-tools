d64d1350d6f886022fa3c6efd70206030dd6a94b
Joel Salisbury <joel.saisbury@gmail.com>
2025-02-13 15:45:21 -0500

    home link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,7 +89,7 @@
 
                             <div class="card-footer">
                                 <a 
-                                href="{{ route('decision_maker.institutional_priorities.index') }}"
+                                href="{{ route('decision_maker.projects.index') }}"
                                 class="tool-link">Learn More &raquo;</a>
                             </div>
 
