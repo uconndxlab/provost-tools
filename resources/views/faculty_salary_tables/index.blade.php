db7a810014958e7db9585082770553b6bbf2f943
Joel Salisbury <joel.saisbury@gmail.com>
2025-06-02 11:55:57 -0400

    new warning for data validity

--- resources/views/faculty_salary_tables/index.blade.php
+++ resources/views/faculty_salary_tables/index.blade.php
@@ -1,20 +1,26 @@
 @extends('layout')
 @section('title', 'Faculty Salary Tables')
 @section('content')
-<div class="py-5">
-    <div class="container">
-        <div class="mb-5">
-            <div class="d-flex align-items-start">
-                <h1>Faculty Salary Tables</h1>
-                @if ( Auth::check() && Auth::user()->can_admin )
-                    <a href="{{ route('admin.faculty_salary_tables.index') }}" class="btn btn-danger ms-auto btn-sm">Manage</a>
-                @endif
-            </div>
-            
-            <p>Compare salaries with faculty in your department.</p>
-        </div>
+    <div class="py-5">
+        <div class="container">
+            <div class="mb-5">
+                <div class="d-flex align-items-start">
+                    <h1>Faculty Salary Tables</h1>
+                    @if (Auth::check() && Auth::user()->can_admin)
+                        <a href="{{ route('admin.faculty_salary_tables.index') }}"
+                            class="btn btn-danger ms-auto btn-sm">Manage</a>
+                    @endif
+                </div>
 
-        <livewire:faculty-salary-tables />
+                <div class="alert alert-info alert-dismissible fade show" role="alert">
+                    <strong>This data was pulled as of 2/11/2025 from Core-CT.</strong>
+                    <br>
+                    Please note, salaries are full-time annual salaries as of 2/11/2025, reported as of 2/11/2025, derived from Core-CT. In other words, any changes or corrections that may have occurred after that date, even if retroactive to be effective before 2/11/2025, will not be reflected. All faculty are assigned to their home academic department, if applicable.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+
+            <livewire:faculty-salary-tables />
+        </div>
     </div>
-</div>
 @endsection
